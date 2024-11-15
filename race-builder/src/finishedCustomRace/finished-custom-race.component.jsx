99a55b6f27ee761864326d3ea9b26b9cacd23125
BrieAnnaAllen <BrieAnnaNAllen@gmail.com>
2024-11-15 11:31:08 -0500

    Finished Basic Functionality
    Added functions to Finished Custom Race to successfully displace all sections of the finished product.

--- race-builder/src/finishedCustomRace/finished-custom-race.component.jsx
+++ race-builder/src/finishedCustomRace/finished-custom-race.component.jsx
@@ -4,14 +4,34 @@ import { Link } from "react-router-dom";
 
 
 const FinishedCustomRace = () => {
-    const {race1, race2, addToCustomRace, updateBalance} = useContext(CustomContext);
+    const {customRace} = useContext(CustomContext);
+    
+    const displayCustomRace = () => {
+      var display = [];
+      Object.keys(customRace).forEach((key) => {
+        console.log("Girl are we even here fr");
 
-
+        if(key === "traits"){
+          display.push(<h2>Traits</h2>);
+          Object.keys(customRace[key]).forEach((key2) => {
+            display.push(<div id={`${key2}`} ><h3>{key2}</h3><p>{customRace[key][key2].desc}</p></div>)
+          })
+        }
+        else {
+          display.push(<div id={`${key}`} ><h3>{key}</h3><p>{customRace[key].desc}</p></div>)
+        }
+      });
+      
+      console.log("Display: ");
+      console.log(display);
+      return display;
+    
+    }
    
     
     return(
         <div>
-          Howdy!
+          {displayCustomRace()}
         </div>
     )
 }
