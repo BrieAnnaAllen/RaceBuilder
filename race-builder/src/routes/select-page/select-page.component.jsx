c1ba4853e5cf2dab0373d240a6895048c82e501c
BrieAnnaAllen <BrieAnnaNAllen@gmail.com>
2024-11-25 11:31:22 -0500

    Changed from Select to List

--- race-builder/src/routes/select-page/select-page.component.jsx
+++ race-builder/src/routes/select-page/select-page.component.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import { RacesContext } from '../../contexts/all-races.context';
 import { useRef } from 'react';
 import { CustomContext } from '../../contexts/custom-race.context';
-import { OptGroup, SelectConfig, SelectContainer, Option, SubmitButton} from './select-page.styles';
+import { ListContainer, SubmitButton, ListConfig, UnorderedListConfig, SubHeader} from './select-page.styles';
 
 const ALL_SUPER_RACES = {
   tieflings: 'tieflings',
@@ -40,33 +40,56 @@ const SetRacesHandler = (t1, t2) => SetRaces(t1, t2);
 }
 
 const OptionCreation = () => {
-  var optgr = [];
+  var listSup = [];
 
   const superKeys = Object.keys(ALL_SUPER_RACES);
-  for (let i = 0; i < superKeys.length; i++) {
-    var opt = [];
+  for(let i = 0; i < superKeys.length; i++) {
+    var list = [];
     races.map((race) => {
       if(superKeys[i] == race.super){
-      opt.push(<Option key={race.id}>{race.name}</Option>)
-    }});
-    optgr.push(<OptGroup label={`${superKeys[i]}`.toUpperCase()}>{opt}</OptGroup>)
+        list.push(<ListConfig key={race.id}>{race.name}</ListConfig>)
+      }});
+      listSup.push(<ul>{superKeys[i]} <ul>{list}</ul></ul>)
   }
-  return optgr;
-  // return races.map((race) => <option key={race.id}>{race.name}</option>)
+
+  return listSup;
 }
+// const OptionCreation = () => {
+//   var optgr = [];
+
+//   const superKeys = Object.keys(ALL_SUPER_RACES);
+//   for (let i = 0; i < superKeys.length; i++) {
+//     var opt = [];
+//     races.map((race) => {
+//       if(superKeys[i] == race.super){
+//       opt.push(<Option key={race.id}>{race.name}</Option>)
+//     }});
+//     optgr.push(<OptGroup label={`${superKeys[i]}`.toUpperCase()}>{opt}</OptGroup>)
+//   }
+//   return optgr;
+//   // return races.map((race) => <option key={race.id}>{race.name}</option>)
+// }
 
   return (
-    <SelectContainer>
-       <SelectConfig ref={select1} size="4">
+    <ListContainer>
+      <div>
+      {OptionCreation()}
+      </div>
+        
+      <SubmitButton onClick={onClickChooseRaces}><Link to='/age'>Select</Link></SubmitButton>
+      <div>
+      {OptionCreation()}
+      </div>
+       {/* <SelectConfig ref={select1}>
         {OptionCreation()}
           {/* {races.map((race) => <option key={race.id}>{race.name}</option>)} */}
-        </SelectConfig>
+        {/* </SelectConfig>
         <SubmitButton onClick={onClickChooseRaces}><Link to='/age'>Select</Link></SubmitButton>
         <SelectConfig  ref={select2}>
         {OptionCreation()}
-        </SelectConfig>
+        </SelectConfig> */}
         
-    </SelectContainer>
+    </ListContainer>
   )
 }
 
--- race-builder/src/routes/select-page/select-page.styles.jsx
+++ race-builder/src/routes/select-page/select-page.styles.jsx
@@ -1,45 +1,65 @@
 import styled from "styled-components";
 import keyframes from "styled-components";
 
-export const OptGroup = styled.optgroup`
-    -moz-appearance: none;
-    -webkit-appearence: none;
-    text-align: center;
-    background: none;
-    
-    
-    
-    
-`;
 
-export const Option = styled.option`
-    text-align: center; 
-    background: none;
-    overflow: auto;
-    
-`;
-
-export const SelectConfig = styled.select`
-    appearance: none;
-    background: none;
-    border: none;
-    font-size: 80px;
-    overflow: auto;
-`;
-
-export const SelectContainer = styled.div`
+export const ListContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     min-height: 100%;
     min-width: 100%;
+`
 
-   // -ms-transform: translateX(-50%) translateY(-50%);
-   // -webkit-transform: translate(-50%, -50%);
-    //transform: translate(-50%, -50%);
-`;
+export const ListConfig = styled.li`
+    
+`
 
+export const UnorderedListConfig = styled.ul`
+    
+`
+
+// export const OptGroup = styled.optgroup`
+//     -moz-appearance: none;
+//     -webkit-appearence: none;
+//     text-align: center;
+//     background: none;
+//     display: none;
+    
+// `;
+
+// export const Option = styled.option`
+//     text-align: center; 
+//     background: none;
+//     overflow: auto;
+    
+    
+// `;
 export const SubmitButton = styled.button`
     background: none;
     border: none;
-`;
+`
+
+// export const SelectConfig = styled.select`
+//     appearance: none;
+//     background: none;
+//     border: none;
+//     font-size: 80px;
+//     overflow: auto;
+//     &:hover ${OptGroup}{
+//         box-shadow: 0 0 10px 100px #1882A8 inset;
+//         display: block;
+//     }
+// `;
+
+// export const SelectContainer = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     min-height: 100%;
+//     min-width: 100%;
+
+//    // -ms-transform: translateX(-50%) translateY(-50%);
+//    // -webkit-transform: translate(-50%, -50%);
+//     //transform: translate(-50%, -50%);
+// `;
+
